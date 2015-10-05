a67db5aa0088f92aa6a64a48d12d74a22990358e
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-10-04 18:49:22 -0700

    rendering of the word

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var utils = require('../lib/utils');
+var wikt = require('enwiktionary-analyzer');
 
-// -----------------------------------------------------------------------------
 
 function render(viewName, layoutPath) {
     return function (req, res) {
@@ -23,11 +23,37 @@ function redirect(url, status) {
 function renderHome(req, res) {
 	var word = req.params.word || req.query.word;
 	var lang = req.params.lang || req.query.lang || "en";
-	res.render('home', {
-		wordQuery:word,
-		langQuery:lang,
-		xxx:"XXXXXX"
-	});
+
+	function finish(err, parsedWord, wordHTMLWiktionary) {
+		res.render('home', {
+			wordQuery:word,
+			langQuery:lang,
+			error:err,
+			word:parsedWord,
+			wordHTMLWiktionary:wordHTMLWiktionary
+		});
+	}
+	if (word && lang) {
+		wikt.api.fetchArticleForLanguageHtml(word, lang, function(err, articleWiktHtml) {
+			if (err) 
+				return finish("Not Found");
+
+			wikt.api.fetchArticle(word, function(err, article) {
+				if (err) 
+					return finish("Not Found");
+				wikt.analyzer.parseArticle(word, article, function(err, parsedWord){
+					if (err)
+						return finish("Problem With Parsing");
+					var parsedWordInLang = parsedWord[lang];
+					if (!parsedWordInLang)
+						return finish("No Such Lang In Word");
+					finish(null, parsedWordInLang, articleWiktHtml);
+				});
+			});
+
+		})
+	}
+	
 }
 
 
