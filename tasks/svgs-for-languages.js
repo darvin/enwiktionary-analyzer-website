ef820ca0f129f14708f4de2a2ddb28736162985b
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-10-07 20:31:05 -0700

    generate files

--- tasks/svgs-for-languages.js
+++ tasks/svgs-for-languages.js
@@ -5,61 +5,90 @@ var analyzer = require('enwiktionary-analyzer').analyzer;
 var fs = require('fs');
 var path = require('path');
 var async = require('async');
-var WIDTH = 30;
 var HEIGHT = 20;
+var WIDTH = HEIGHT*2;
+var WIDTH_LONG = HEIGHT*4;
+
 var helpers = require('../lib/helpers');
 
 var languages = analyzer.languagesList();
 var imagesDir = path.join(__dirname, '..', 'build', 'images');
 
 
-function generateIcon(lang, callback) {
 
-  var langLabel = helpers.wordLinkLangIconLabel(lang);
+var generateIcon = function(){
+  var raphael = null;
+  var win = null;
   jsdom.env(
     "<html></html>",
     [],
-    function (errors, win) {
+    function (errors, theWin) {
       if (errors) {
         throw errors;
       } else {
+        win = theWin;
         global.window = win
         global.document = win.document
         global.navigator = win.navigator
 
-        var raphael = require('raphael')
+        raphael = require('raphael')
         raphael.setWindow(win)
 
 
 
-        var paper = raphael(0, 0, WIDTH, HEIGHT);
-        
-        var rect = paper.rect(0, 0, WIDTH, HEIGHT).attr("fill", "#444");
-        var text = paper.text("50%", "69%", langLabel).attr({ 
-          "font-size": HEIGHT/1.25, 
-          "font-family": "Verdana, Geneva, sans-serif", 
-          "font-weight": "bold",
-          "letter-spacing":"-1px",
-          "fill":"#fff"
-        });
 
-        svg = win.document.documentElement.innerHTML
-        svg = svg.replace('<head></head><body>', '')
-        svg = svg.replace('<head></head><body style="">', '')
-        svg = svg.replace('</body>', '')
-
-        fs.writeFile(path.join(imagesDir, lang+'.svg'), svg, 'utf8', function(err, result) {
-          if (err) {
-            console.log("Error writing: ", err);
-            throw err;
-          }
-          console.log("- generated: "+lang+'.svg');
-          callback(null);
-        });
       }
     }
   );
-}
+  function clearPaper(paper){
+    var paperDom = paper.canvas;
+    paperDom.parentNode.removeChild(paperDom);
+  }
+
+  function widthForLabel(label) {
+    if (label.length<=4) {
+      return WIDTH;
+    } else {
+      return WIDTH_LONG;
+    }
+  }
+
+  return function(lang, callback) {
+    var langLabel = helpers.wordLinkLangIconLabel([lang, null]);
+
+    var width = widthForLabel(langLabel);
+    var paper = raphael(0, 0, width, HEIGHT);
+    
+    var rect = paper.rect(0, 0, width, HEIGHT).attr("fill", "#444");
+    var text = paper.text("50%", "69%", langLabel).attr({ 
+      "font-size": HEIGHT*0.75, 
+      "font-family": "Verdana, Geneva, sans-serif", 
+      "font-weight": "bold",
+      "letter-spacing":"-1px",
+      "fill":"#fff"
+    });
+
+    svg = win.document.documentElement.innerHTML
+    clearPaper(paper);
+    svg = svg.replace('<head></head><body>', '')
+    svg = svg.replace('<head></head><body style="">', '')
+    svg = svg.replace('</body>', '')
+
+    fs.writeFile(path.join(imagesDir, lang+'.svg'), svg, 'utf8', function(err, result) {
+      if (err) {
+        console.log("Error writing: ", err);
+        throw err;
+      }
+      // console.log("- generated: "+lang+'.svg  ');
+      if (langLabel.length>3) {
+        console.log("Lang: ", lang, " Lang Label: ",langLabel, " Name: ", helpers.wordLinkLangFull([lang, null]));
+      }
+      callback(null);
+    });
+  }
+}();
+
+
 var mkdirp = require('mkdirp');
 
 module.exports = function(grunt) {
@@ -72,8 +101,12 @@ module.exports = function(grunt) {
     var done = this.async();
 
     mkdirp(imagesDir, function(err) { 
-
-      async.eachSeries(analyzer.languagesList(), generateIcon, done);
+      var allLangs = analyzer.languagesList();
+      var indexHTML = "<html><body>"+allLangs.splice(0,2000).map(function(lang){
+        return '   <img src="'+lang+'.svg" alt="'+lang+'"/>'
+      }).join("\n")+"</body></html>"
+      fs.writeFileSync(path.join(imagesDir, "index.html"), indexHTML, "utf8");
+      async.eachSeries(allLangs, generateIcon, done);
     });
   });
 
